9ac2f9d09bb879727f8d6bf20e897c803face508
Wheslley <wheslley_rimar@hotmail.com>
2020-06-18 09:46:59 -0300

    Java - Spring

---
+++ hotel/src/main/java/generation/hotel/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package generation.hotel.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import generation.hotel.model.Usuario;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);  
+}
